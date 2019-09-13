7ceaebd82fed8beecca65064d70ea599cb2e243f
egracheva <ekaterina.s.gracheva@gmail.com>
2019-09-13 14:59:44 +0900

    Added ModelNoTrain into the model.py

--- SMILESX/smimodel.py
+++ SMILESX/smimodel.py
@@ -10,6 +10,7 @@ from keras.engine.topology import Layer
 from keras.utils import multi_gpu_model
 
 from keras import backend as K
+from keras import initializers
 # import tensorflow as tf
 
 # #from keras.backend.tensorflow_backend import set_session
@@ -113,33 +114,6 @@ class LSTMAttModel():
 
         return model
 ##
-
-## Function to fit a model on a multi-GPU machine
-class ModelMGPU(Model):
-    # Initialization
-    # ser_model: based model to pass to >1 GPUs
-    # gpus: number of GPUs
-    # bridge_type: optimize for bridge types (NVLink or not) 
-    # returns:
-    #         a multi-GPU model (based model copied to GPUs, batch is splitted over the GPUs)
-    def __init__(self, ser_model, gpus, bridge_type):
-        if bridge_type == 'NVLink':
-            pmodel = multi_gpu_model(ser_model, gpus, cpu_merge=False) # recommended for NV-link
-        else:
-            pmodel = multi_gpu_model(ser_model, gpus)
-        self.__dict__.update(pmodel.__dict__)
-        self._smodel = ser_model
-
-    def __getattribute__(self, attrname):
-        '''Override load and save methods to be used from the serial-model. The
-        serial-model holds references to the weights in the multi-gpu model.
-        '''
-        # return Model.__getattribute__(self, attrname)
-        if 'load' in attrname or 'save' in attrname:
-            return getattr(self._smodel, attrname)
-
-        return super(ModelMGPU, self).__getattribute__(attrname)
-##
 ##
 class AttentionMNoTrain(Layer):
     """
@@ -203,7 +177,7 @@ class AttentionMNoTrain(Layer):
         return super(AttentionMNoTrain, self).get_config()
 ##
 
-## Neural architecture of the SMILES-X for the optimization step
+## Neural architecture of the SMILES-X for the trainless geometry optimization step
 class LSTMAttModelNoTrain():
     # Initialization
     # inputtokens: maximum length for the encoded and tokenized SMILES
@@ -244,4 +218,31 @@ class LSTMAttModelNoTrain():
         model = Model(inputs=input_, outputs=net)
 
         return model
+##
+
+## Function to fit a model on a multi-GPU machine
+class ModelMGPU(Model):
+    # Initialization
+    # ser_model: based model to pass to >1 GPUs
+    # gpus: number of GPUs
+    # bridge_type: optimize for bridge types (NVLink or not) 
+    # returns:
+    #         a multi-GPU model (based model copied to GPUs, batch is splitted over the GPUs)
+    def __init__(self, ser_model, gpus, bridge_type):
+        if bridge_type == 'NVLink':
+            pmodel = multi_gpu_model(ser_model, gpus, cpu_merge=False) # recommended for NV-link
+        else:
+            pmodel = multi_gpu_model(ser_model, gpus)
+        self.__dict__.update(pmodel.__dict__)
+        self._smodel = ser_model
+
+    def __getattribute__(self, attrname):
+        '''Override load and save methods to be used from the serial-model. The
+        serial-model holds references to the weights in the multi-gpu model.
+        '''
+        # return Model.__getattribute__(self, attrname)
+        if 'load' in attrname or 'save' in attrname:
+            return getattr(self._smodel, attrname)
+
+        return super(ModelMGPU, self).__getattribute__(attrname)
 ##
