200b6b2e0b75a3959668c376fd912266169750b5
egracheva <ekaterina.s.gracheva@gmail.com>
2019-12-12 09:20:15 +0900

    Added fold_of_interest, in case the user doesn't want to take each of the folds into account. Added kfolds functionality (need to add runs, too). Put off the mean computation out of the loop. Also moved tokens padding addition before the loop.

--- SMILESX/inference.py
+++ SMILESX/inference.py
@@ -31,6 +31,7 @@ def Inference(data_name,
               smiles_list = ['CC','CCC','C=O'], 
               data_units = '',
               k_fold_number = 8,
+              folds_of_interest = False,
               augmentation = False, 
               outdir = "./data/"):
     
@@ -90,51 +91,63 @@ def Inference(data_name,
     # Tokens as a list
     tokens = token.get_vocab(input_dir+data_name+'_Vocabulary.txt')
 
+    # Add 'pad', 'unk' tokens to the existing list
+    vocab_size = len(tokens)
+    tokens, vocab_size = token.add_extra_tokens(tokens, vocab_size)
+
+    # Transformation of tokenized SMILES to vector of intergers and vice-versa
+    token_to_int = token.get_tokentoint(tokens)
+    int_to_token = token.get_inttotoken(tokens)
+
     # models ensembling
     smiles_y_pred_mean_array = np.empty(shape=(0,len(smiles_checked)), dtype='float')
-    for ifold in range(k_fold_number):        
-        # Add 'pad', 'unk' tokens to the existing list
-        vocab_size = len(tokens)
-        tokens, vocab_size = token.add_extra_tokens(tokens, vocab_size)
 
-        # Transformation of tokenized SMILES to vector of intergers and vice-versa
-        token_to_int = token.get_tokentoint(tokens)
-        int_to_token = token.get_inttotoken(tokens)
-        
-        # Best architecture to visualize from
-        model_train = load_model(input_dir+'LSTMAtt_'+data_name+'_model.best_fold_'+str(ifold)+'.hdf5', 
-                                 custom_objects={'AttentionM': model.AttentionM()})
+    # If there is a list of folds of interest defined by the user
+    if folds_of_interest:
+        folds = folds_of_interest
+    # If there is no defined list of the folds of interest, run for all folds
+    else:
+        folds = [n for n in range(0, k_fold_number)]
+    kfold = KFold(k_fold_number, shuffle = True)
+    ifold = 0
 
-        if ifold == 0:
-            # Maximum of length of SMILES to process
-            max_length = model_train.layers[0].output_shape[-1]
-            print("Full vocabulary: {}\nOf size: {}\n".format(tokens, vocab_size))
-            print("Maximum length of tokenized SMILES: {} tokens\n".format(max_length))
-
-        model_train.compile(loss="mse", optimizer='adam', metrics=[metrics.mae,metrics.mse])
-
-        # predict and compare for the training, validation and test sets
-        smiles_x_enum_tokens_tointvec = token.int_vec_encode(tokenized_smiles_list = smiles_x_enum_tokens, 
-                                                             max_length = max_length, 
-                                                             vocab = tokens)
-
-        smiles_y_pred = model_train.predict(smiles_x_enum_tokens_tointvec)
-
-        # compute a mean per set of augmented SMILES
-        smiles_y_pred_mean, _ = utils.mean_median_result(smiles_x_enum_card, smiles_y_pred)
-        
-        smiles_y_pred_mean_array = np.append(smiles_y_pred_mean_array, smiles_y_pred_mean.reshape(1,-1), axis = 0)
-        
-        if ifold == (k_fold_number-1):
-            smiles_y_pred_mean_ensemble = np.mean(smiles_y_pred_mean_array, axis = 0)
-            smiles_y_pred_sd_ensemble = np.std(smiles_y_pred_mean_array, axis = 0)
-
-            pred_from_ens = pd.DataFrame(data=[smiles_x,
-                                               smiles_y_pred_mean_ensemble,
-                                               smiles_y_pred_sd_ensemble]).T
-            pred_from_ens.columns = ['SMILES', 'ens_pred_mean', 'ens_pred_sd']
+    for train_val_idx, test_idx in kfold.split(data.smiles):
+        if ifold in folds:    
             
-            print("***Inference of SMILES property done.***")
             
-            return pred_from_ens
+            # Best architecture to visualize from
+            model_train = load_model(input_dir+'LSTMAtt_'+data_name+'_model.best_fold_'+str(ifold)+'.hdf5', 
+                                     custom_objects={'AttentionM': model.AttentionM(seed=5)})
+
+            if ifold == 0:
+                # Maximum of length of SMILES to process
+                max_length = model_train.layers[0].output_shape[-1]
+                print("Full vocabulary: {}\nOf size: {}\n".format(tokens, vocab_size))
+                print("Maximum length of tokenized SMILES: {} tokens\n".format(max_length))
+
+            # predict and compare for the training, validation and test sets
+            smiles_x_enum_tokens_tointvec = token.int_vec_encode(tokenized_smiles_list = smiles_x_enum_tokens, 
+                                                                 max_length = max_length, 
+                                                                 vocab = tokens)
+
+            smiles_y_pred = model_train.predict(smiles_x_enum_tokens_tointvec)
+
+            # compute a mean per set of augmented SMILES
+            smiles_y_pred_mean, _ = utils.mean_median_result(smiles_x_enum_card, smiles_y_pred)
+            
+            smiles_y_pred_mean_array = np.append(smiles_y_pred_mean_array, smiles_y_pred_mean.reshape(1,-1), axis = 0)
+        else:
+            ifold += 1
+
+    smiles_y_pred_mean_ensemble = np.mean(smiles_y_pred_mean_array, axis = 0)
+    smiles_y_pred_sd_ensemble = np.std(smiles_y_pred_mean_array, axis = 0)
+
+    pred_from_ens = pd.DataFrame(data=[smiles_x,
+                                       smiles_y_pred_mean_ensemble,
+                                       smiles_y_pred_sd_ensemble]).T
+    pred_from_ens.columns = ['SMILES', 'ens_pred_mean', 'ens_pred_sd']
+    
+    print("***Inference of SMILES property done.***")
+    
+    return pred_from_ens
 ##
